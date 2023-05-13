e5c9f511163d2c0d9f90004ab426c4e0cefd3b81
Alex Devia <famunozd@udistrital.edu.co>
2023-05-13 13:41:27 -0500

    API delete,create

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bicicletaRouter = require('./routes/bicicletas');
+var bicicletaAPIRouter = require('./routes/api/bicicleta');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bicicletas', bicicletaRouter);
+app.use('/api/bicicletas',bicicletaAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
